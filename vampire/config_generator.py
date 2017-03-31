2d3e93e7df2a4a5f234b04f4e5ab27c2573f0abc
Rochelle O'Hagan <spatialexplore@gmail.com>
2017-03-31 11:49:10 +1100

    Moved generate_config_file to config_generator.py

--- vampire/config_generator.py
+++ vampire/config_generator.py
@@ -1,7 +1,8 @@
 import errno
 import MODISConfigFactory
+import CHIRPSConfigFactory
 
-def generate_config_file(self, output_file, params):
+def generate_config_file(output_file, params):
     try:
         pfile = open(output_file, 'a')
     except IOError as e:
@@ -11,9 +12,10 @@ def generate_config_file(self, output_file, params):
         raise
     else:
         with pfile:
-            cf = MODISConfigFactory.MODISConfigFactory(name='cf', country=params['country'],
+            cf = CHIRPSConfigFactory.CHIRPSConfigFactory(name='cf')
+            mf = MODISConfigFactory.MODISConfigFactory(name='mf', country=params['country'],
                                                        start_date=params['start_date'], end_date=None)
-            pfile.write(self.generate_header_directory())
+            pfile.write(cf.generate_header_directory())
             if 'product' in params:
                 if params['product'] == "rainfall anomaly":
                     print "Not currently supported"
@@ -22,17 +24,17 @@ def generate_config_file(self, output_file, params):
                     # pfile.write(self.generate_rainfall_anomaly_config(params['country'], params['interval'],
                     #                                                params['start_date'], params['output']))
                 elif params['product'] == "vhi":
-                    pfile.write(cf.generate_header_run())
-#                    pfile.write(cf.generate_vci_config(params['country'], params['interval'],
+                    pfile.write(mf.generate_header_run())
+#                    pfile.write(mf.generate_vci_config(params['country'], params['interval'],
 #                                                       params['start_date'], params['output']))
-                    pfile.write(cf.generate_tci_config(params['country'], params['interval'],
+                    pfile.write(mf.generate_tci_config(params['country'], params['interval'],
                                                        params['start_date'], params['output']))
-#                    pfile.write(cf.generate_vhi_config(params['country'], params['interval'],
+#                    pfile.write(mf.generate_vhi_config(params['country'], params['interval'],
 #                                                       params['start_date'], params['output']))
                 elif params['product'] == "rainfall_longterm_average":
-                    pfile.write(self.generate_header_chirps())
-                    pfile.write(self.generate_header_run())
-                    pfile.write(self.generate_rainfall_long_term_average_config(params['country'],
+                    pfile.write(cf.generate_header_chirps())
+                    pfile.write(cf.generate_header_run())
+                    pfile.write(cf.generate_rainfall_long_term_average_config(params['country'],
                                                                                 params['interval']
                                                                                 ))
             pfile.close()
