76b295595f9190a451d597e5625901cd86951295
rochelle <spatialexplore@gmail.com>
2017-05-09 05:11:28 +0530

    Fixes to calculations to use masked arrays correctly.

--- vampire/processing/calculate_statistics_os.py
+++ vampire/processing/calculate_statistics_os.py
@@ -15,8 +15,10 @@ def calc_average(file_list, avg_file):
                     first = False
                 cur_a = cur_r.read(1, masked=True)
                 arrayList.append(cur_a)
-        dst_a = np.dstack(arrayList)
-        dst_r = np.mean(dst_a, axis=2)
+        dst_a = np.ma.dstack(arrayList)
+        dst_r = dst_a.mean(axis=2, dtype=rasterio.float32)
+        dst_r = dst_r.filled(-9999)
+        profile.update(dtype=rasterio.float32, nodata=-9999)
         with rasterio.open(avg_file, 'w', **profile) as dst:
             dst.write(dst_r.astype(rasterio.float32), 1)
     return None
@@ -34,10 +36,10 @@ def calc_min(file_list, min_file):
                     first = False
                 cur_a = cur_r.read(1, masked=True)
                 arrayList.append(cur_a)
-        dst_a = np.dstack(arrayList)
-        dst_r = np.amin(dst_a, axis=2)
+        dst_a = np.ma.dstack(arrayList)
+        dst_r = np.ma.min(dst_a, axis=2)
         with rasterio.open(min_file, 'w', **profile) as dst:
-            dst.write(dst_r.astype(rasterio.float32), 1)
+            dst.write(dst_r.astype(profile.dtype), 1)
             print "saved minimum in: ", min_file
     return None
 
@@ -54,10 +56,10 @@ def calc_max(file_list, max_file):
                     first = False
                 cur_a = cur_r.read(1, masked=True)
                 arrayList.append(cur_a)
-        dst_a = np.dstack(arrayList)
-        dst_r = np.amax(dst_a, axis=2)
+        dst_a = np.ma.dstack(arrayList)
+        dst_r = np.ma.max(dst_a, axis=2)
         with rasterio.open(max_file, 'w', **profile) as dst:
-            dst.write(dst_r.astype(rasterio.float32), 1)
+            dst.write(dst_r.astype(profile.dtype), 1)
 #            print "saved maximum in: ", max_file
     return None
 
@@ -74,8 +76,9 @@ def calc_std_dev(file_list, sd_file):
                     first = False
                 cur_a = cur_r.read(1, masked=True)
                 arrayList.append(cur_a)
-        dst_a = np.dstack(arrayList)
-        dst_r = np.std(dst_a, axis=2, ddof=1)
+        dst_a = np.ma.dstack(arrayList)
+        dst_r = np.ma.std(dst_a, axis=2, ddof=1)
+        profile.update(dtype=rasterio.float32)
         with rasterio.open(sd_file, 'w', **profile) as dst:
             dst.write(dst_r.astype(rasterio.float32), 1)
 #            print "saved standard deviation in: ", sd_file
@@ -109,7 +112,9 @@ def calc_average_of_day_night(day_file, night_file, avg_file):
         day_a = day_r.read(1, masked=True)
         with rasterio.open(night_file) as night_r:
             night_a = night_r.read(1, masked=True)
-            dst_r = np.mean((day_a, night_a), axis=0)
+            dst_r = np.ma.array((day_a, night_a)).mean(axis=0, dtype=rasterio.float32)
+            dst_r = dst_r.filled(-9999)
+            profile.update(dtype=rasterio.float32, nodata=-9999)
             with rasterio.open(avg_file, 'w', **profile) as dst:
-                dst.write(dst_r.astype(rasterio.uint16), 1)
+                dst.write(dst_r.astype(rasterio.float32), 1)
     return None
