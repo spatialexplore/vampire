b30358d679a18ae14f15c266793e6ad00483e83f
rochelle <spatialexplore@gmail.com>
2017-06-05 09:04:31 +0530

    Add support for directory/patterns for hazard, boundary and output files as well as actual filename.

--- vampire/processing/ImpactAnalysis.py
+++ vampire/processing/ImpactAnalysis.py
@@ -13,6 +13,7 @@ try:
     import calculate_statistics_arc as calculate_statistics
 except ImportError:
     import impact_analysis_os as impact_analysis
+    import calculate_statistics_os as calculate_statistics
 
 
 class ImpactAnalysis():
@@ -22,55 +23,98 @@ class ImpactAnalysis():
         return
 
     def calculate_impact_popn(self, hazard_raster, hazard_dir, hazard_pattern, threshold,
-                                      population_raster, boundary, b_field, output_file,
-                                      hazard_var='vhi'):
+                              population_raster, boundary, b_field, output_file,
+                              output_dir, output_pattern, hazard_var='vhi'):
         if threshold is None:
             # get threshold from VampireDefaults
             _threshold = self.vampire.get('hazard_impact', '{0}_threshold'.format(hazard_var))
         else:
             _threshold = threshold
 
+        if hazard_raster is None:
+            if hazard_pattern is not None:
+                _input_files = directory_utils.get_matching_files(hazard_dir, hazard_pattern)
+                _hazard_raster = os.path.join(hazard_dir, _input_files[0])
+            else:
+                raise ValueError("Hazard raster is not specified")
+        else:
+            _hazard_raster = hazard_raster
+
+        if output_file is None:
+            if output_pattern is not None:
+                _input_pattern = self.vampire.get('MODIS_VHI', 'vhi_pattern')
+                _output_file = filename_utils.generate_output_filename(os.path.basename(_hazard_raster),
+                                                                       _input_pattern, output_pattern)
+                _output_file = os.path.join(output_dir, _output_file)
+                if not os.path.exists(output_dir):
+                    os.makedirs(output_dir)
+            else:
+                raise ValueError("No output specified")
+        else:
+            _output_file = output_file
 
         # reclassify hazard raster to generate mask of all <= threshold
-        _reclass_raster = os.path.join(os.path.dirname(output_file), 'hazard_popn_reclass.tif')
-        impact_analysis.reclassify_raster(raster=hazard_raster, threshold=_threshold, output_raster=_reclass_raster)
+        _reclass_raster = os.path.join(os.path.dirname(_output_file), 'hazard_popn_reclass.tif')
+        impact_analysis.reclassify_raster(raster=_hazard_raster, threshold=_threshold, output_raster=_reclass_raster)
 
         if population_raster is None:
             _hazard_raster = _reclass_raster
         else:
             # calculate population from hazard raster and population raster intersection
-            _hazard_raster = os.path.join(os.path.dirname(output_file), 'hazard_popn.tif')
+            _hazard_raster = os.path.join(os.path.dirname(_output_file), 'hazard_popn.tif')
             impact_analysis.multiply_by_mask(raster=population_raster, mask=_reclass_raster,
                                              output_raster=_hazard_raster)
         # calculate impact on boundary
         calculate_statistics.calc_zonal_statistics(raster_file=_hazard_raster, polygon_file=boundary,
-                                                   zone_field=b_field, output_table=output_file)
+                                                   zone_field=b_field, output_table=_output_file)
 
         # add field to table and calculate total for each area
         if population_raster is None:
-            impact_analysis.calc_field(table_name=output_file, new_field='pop_aff', cal_field='COUNT', type='LONG')
+            impact_analysis.calc_field(table_name=_output_file, new_field='pop_aff', cal_field='COUNT', type='LONG')
         else:
-            impact_analysis.calc_field(table_name=output_file, new_field='pop_aff', cal_field='SUM', type='LONG')
+            impact_analysis.calc_field(table_name=_output_file, new_field='pop_aff', cal_field='SUM', type='LONG')
 
         return None
 
     def calculate_impact_area(self, hazard_raster, hazard_dir, hazard_pattern, threshold,
-                              boundary, b_field, output_file, hazard_var='vhi'):
+                              boundary, b_field, output_file, output_dir, output_pattern, hazard_var='vhi'):
         if threshold is None:
             # get threshold from VampireDefaults
             _threshold = self.vampire.get('hazard_impact', '{0}_threshold'.format(hazard_var))
         else:
             _threshold = threshold
 
+        if hazard_raster is None:
+            if hazard_pattern is not None:
+                _input_files = directory_utils.get_matching_files(hazard_dir, hazard_pattern)
+                _hazard_raster = os.path.join(hazard_dir, _input_files[0])
+            else:
+                raise ValueError("Hazard raster is not specified")
+        else:
+            _hazard_raster = hazard_raster
+
+        if output_file is None:
+            if output_pattern is not None:
+                _input_pattern = self.vampire.get('MODIS_VHI', 'vhi_pattern')
+                _output_file = filename_utils.generate_output_filename(os.path.basename(_hazard_raster),
+                                                                       _input_pattern, output_pattern)
+                _output_file = os.path.join(output_dir, _output_file)
+                if not os.path.exists(output_dir):
+                    os.makedirs(output_dir)
+            else:
+                raise ValueError("No output specified")
+        else:
+            _output_file = output_file
+
         # reclassify hazard raster to generate mask of all <= threshold
-        _reclass_raster = os.path.join(os.path.dirname(output_file), 'hazard_area_reclass.tif')
-        impact_analysis.reclassify_raster(raster=hazard_raster, threshold=_threshold, output_raster=_reclass_raster)
+        _reclass_raster = os.path.join(os.path.dirname(_output_file), 'hazard_area_reclass.tif')
+        impact_analysis.reclassify_raster(raster=_hazard_raster, threshold=_threshold, output_raster=_reclass_raster)
 
         # calculate impact on boundary
-        calculate_statistics.calc_zonal_statistics(raster_file=_reclass_raster, polygon_file=boundary,
-                                                   zone_field=b_field, output_table=output_file)
+        stats = calculate_statistics.calc_zonal_statistics(raster_file=_reclass_raster, polygon_file=boundary,
+                                                           zone_field=b_field, output_table=_output_file)
         # convert to hectares
         # TODO: get multiplier from defaults depending on resolution of hazard raster
-        impact_analysis.calc_field(output_file, 'area_aff', 'COUNT', 6.25)
+        impact_analysis.calc_field(_output_file, 'area_aff', 'COUNT', 6.25)
 
         return None
