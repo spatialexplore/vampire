d86d9cf92491fa5932125dcae46c6f0c4b610406
rochelle <spatialexplore@gmail.com>
2017-06-30 16:12:20 +0530

    Added calculate_crop_impact and shapefile_to_table.

--- vampire/processing/impact_analysis_os.py
+++ vampire/processing/impact_analysis_os.py
@@ -1,11 +1,67 @@
 import rasterio
 import rasterstats
 import gdal
+import geopandas
 import numpy as np
 import dbfpy.dbf
 import raster_utils
 import os
 import csv
+import ogr
+import vampire.csv_utils as csv_utils
+import calculate_statistics_os as calculate_statistics
+
+def calculate_crop_impact(hazard_raster, threshold, hazard_var,
+                          crop_boundary, crop_field,
+                          admin_boundary, admin_field,
+                          output_file,
+                          start_date, end_date, intersect=False):
+
+    _output_dir = os.path.dirname(output_file)
+    # reclassify hazard raster to generate mask of all <= threshold
+    _reclass_raster = os.path.join(os.path.dirname(output_file), 'hazard_crops_reclass.tif')
+    reclassify_raster(raster=hazard_raster, threshold=threshold, output_raster=_reclass_raster)
+
+    _zone_table = os.path.join(_output_dir, 'hazard_crops_table.csv')
+
+    # calculate impact on boundary
+    # if have admin boundary as well, intersect the two boundaries first, then dissolve after the join to
+    # calculate crop impact per admin area
+    if admin_boundary is None:
+        _boundary = crop_boundary
+        _zone_field = crop_field
+    else:
+        if intersect:
+            _boundary_output = os.path.join(_output_dir, 'crop_admin_intersection.shp')
+            intersect_boundaries([crop_boundary, admin_boundary], _boundary_output)
+            _boundary = _boundary_output
+            _zone_field = 'fid'
+        else:
+            _boundary = crop_boundary
+            _zone_field = 'fid'
+    stats = calculate_statistics.calc_zonal_statistics(raster_file=_reclass_raster, polygon_file=_boundary,
+                                                       zone_field=_zone_field, output_table=_zone_table)
+
+    # convert to hectares
+    # TODO: get multiplier from defaults depending on resolution of hazard raster
+    csv_utils.calc_field(table_name=_zone_table, new_field='area_aff', cal_field='COUNT', multiplier=6.25)
+    # add start and end date fields and set values
+    csv_utils.add_field(table_name=_zone_table, new_field='start_date', value=start_date)
+    csv_utils.add_field(table_name=_zone_table, new_field='end_date', value=end_date)
+
+    # calculate affected crops within admin areas
+    # join table to boundary, then extract district etc.
+    if admin_boundary is not None:
+        #            _boundary_table = os.path.join(os.path.dirname(_output_file), 'crop_admin_table.csv')
+        #            impact_analysis.shapefile_to_table(_boundary, _boundary_table)
+        #            _merge_output = os.path.join(os.path.dirname(_output_file), 'crop_hazard_merge.csv')
+        #            csv_utils.merge_files(file1=_zone_table, file2=_boundary_table, output_file=_merge_output,
+        #                                      file1_field='FID_', file2_field='FID_PADDY_')
+        _merge_output = _zone_table
+        _out_dict = {'area_aff': 'sum'}
+        csv_utils.aggregate_on_field(input=_merge_output, ref_field=admin_field,
+                                     output_fields_dict=_out_dict, output=output_file, all_fields=True)
+    return None
 
 def reclassify_raster(raster, threshold, output_raster):
     _threshold = int(threshold)
@@ -55,3 +111,67 @@ def multiply_by_mask(raster, mask, output_raster):
                 print "saved multiply result in: ", output_raster
     return None
 
+def shapefile_to_table(input_file, output_file):
+    if os.path.splitext(output_file)[1] != '.dbf':
+        _output = '{0}{1}'.format(os.path.splitext(os.path.basename(output_file))[0], '.dbf')
+        _output_csv = '{0}{1}'.format(os.path.splitext(output_file)[0], '.csv')
+    else:
+        _output = os.path.basename(output_file)
+        _output_csv = '{0}{1}'.format(os.path.splitext(output_file)[0], '.csv')
+    _csv_file = open(_output_csv, 'wb')
+    _ds = ogr.Open(input_file)
+    _layer = _ds.GetLayer()
+    _dfn = _layer.GetLayerDefn()
+    _nfields = _dfn.GetFieldCount()
+    fields = []
+    for i in range(_nfields):
+        fields.append(_dfn.GetFieldDefn(i).GetName())
+    _csv_writer = csv.DictWriter(_csv_file, fields)
+    _csv_writer.writeheader()
+    for feat in _layer:
+        _attributes = feat.items()
+        _csv_writer.writerow(_attributes)
+
+    # clean up
+    del _csv_writer, _layer, _ds
+    _csv_file.close()
+    #    arcpy.TableToTable_conversion(input, os.path.dirname(output), os.path.splitext(_output)[0])
+#    vampire.csv_utils.convert_dbf_to_csv(os.path.join(os.path.dirname(output), _output), _output_csv)
+    return None
+
+def intersect_boundaries(boundary_list, boundary_output):
+    _g_list = []
+    for b in boundary_list:
+        _df = geopandas.GeoDataFrame.from_file(b)
+        _g_list.append(_df)
+
+#    g1 = geopandas.GeoDataFrame.from_file("crime_stat.shp")
+#    g2 = geopandas.GeoDataFrame.from_file("population.shp")
+    _new_data = []
+    _new_columns = []
+    _res_intersection = geopandas.overlay(_g_list[0], _g_list[1], how='intersection')
+
+    for index, _data in _g_list[0].iterrows():
+        for index2, _data2 in _g_list[1].iterrows():
+            if _data['geometry'].intersects(_data2['geometry']):
+                _new_dict = {}
+                for x in list(_data.columns.values):
+                    _new_dict[x] = _data[x]
+                    _new_columns.append(x)
+                for y in list(_data2.columns.values):
+                    if _new_dict.has_key(y):
+                        _new_key = '{0}_data2'.format(y)
+                        _new_dict[_new_key] = _data2[y]
+                        _new_columns.append(_new_key)
+                    else:
+                        _new_dict[y] = _data2[y]
+                _new_dict['geometry'] = _data['geometry'].intersection(_data2['geometry'])
+                _new_columns.append('geometry')
+#               _new_dict['area'] = _data['geometry'].intersection(_data2['geometry']).area
+                _new_data.append(_new_dict)
+
+    df = geopandas.GeoDataFrame(_new_data,columns=_new_columns)
+    df.to_file(boundary_output)
+    df.head()
+#    arcpy.Intersect_analysis(in_features=boundary_list, out_feature_class=boundary_output)
+    return None
