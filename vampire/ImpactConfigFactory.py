1b5fb08bc452aa3df75504d74600daf500c87ee0
rochelle <spatialexplore@gmail.com>
2017-06-30 16:11:24 +0530

    Add start & end date to impact so can be stored in .csv output. Changed crop impact to call function in impact_analysis_os/arc

--- vampire/ImpactConfigFactory.py
+++ vampire/ImpactConfigFactory.py
@@ -24,7 +24,8 @@ class ImpactConfigFactory(ConfigFactory.ConfigFactory):
 
     def _generate_area_impact_section(self, hazard_file, hazard_dir, hazard_pattern,
                                       boundary_file, boundary_dir, boundary_pattern, boundary_field,
-                                      output_file, output_dir, output_pattern):
+                                      output_file, output_dir, output_pattern,
+                                      start_date, end_date):
         file_string = """
     # calculate area impact (ha)
     - process: impact
@@ -54,12 +55,19 @@ class ImpactConfigFactory(ConfigFactory.ConfigFactory):
       output_dir: {output_dir}
       output_pattern: '{output_pattern}'
       """.format(output_dir=output_dir, output_pattern=output_pattern)
+        if start_date is not None:
+            file_string += """
+      start_date: {start_date}""".format(start_date=start_date)
+        if end_date is not None:
+            file_string += """
+      end_date: {end_date}""".format(end_date=end_date)
         return file_string
 
     def _generate_popn_impact_section(self, hazard_file, hazard_dir, hazard_pattern,
                                       boundary_file, boundary_dir, boundary_pattern, boundary_field,
                                       population_file,
-                                      output_file, output_dir, output_pattern):
+                                      output_file, output_dir, output_pattern,
+                                      start_date, end_date):
         file_string = """
     # calculate population impact (number of people)
     - process: impact
@@ -92,7 +100,67 @@ class ImpactConfigFactory(ConfigFactory.ConfigFactory):
       output_dir: {output_dir}
       output_pattern: '{output_pattern}'
         """.format(output_dir=output_dir, output_pattern=output_pattern)
+        if start_date is not None:
+            file_string += """
+      start_date: {start_date}""".format(start_date=start_date)
+        if end_date is not None:
+            file_string += """
+      end_date: {end_date}""".format(end_date=end_date)
         return file_string
+
+    def _generate_crop_impact_section(self, hazard_file, hazard_dir, hazard_pattern,
+                                      crop_boundary, crop_dir, crop_pattern, crop_field,
+                                      admin_boundary, admin_boundary_dir, admin_boundary_pattern, admin_field,
+                                      output_file, output_dir, output_pattern,
+                                      start_date, end_date):
+        file_string = """
+    # calculate crop impact (ha)
+    - process: impact
+      type: crops"""
+        if hazard_file is not None:
+            file_string += """
+      hazard_file: {hazard_file}""".format(hazard_file=hazard_file)
+        else:
+            file_string += """
+      hazard_dir: {hazard_dir}
+      hazard_pattern: '{hazard_pattern}'""".format(hazard_dir=hazard_dir, hazard_pattern=hazard_pattern)
+        if crop_boundary is not None:
+            file_string += """
+      crop_boundary: {crop_boundary}""".format(crop_boundary=crop_boundary)
+        else:
+            file_string += """
+      crop_dir: {crop_dir}
+      crop_pattern: '{crop_pattern}'""".format(crop_dir=crop_dir, crop_pattern=crop_pattern)
+        if crop_field is not None:
+            file_string += """
+      crop_field: {crop_field}""".format(crop_field=crop_field)
+        if admin_boundary is not None:
+            file_string += """
+      boundary_file: {admin_boundary_file}""".format(admin_boundary_file=admin_boundary)
+        else:
+            file_string += """
+      boundary_dir: {admin_boundary_dir}
+      boundary_pattern: '{admin_boundary_pattern}'""".format(admin_boundary_dir=admin_boundary_dir,
+                                                                     admin_boundary_pattern=admin_boundary_pattern)
+        file_string += """
+      boundary_field: {admin_field}""".format(admin_field=admin_field)
+
+        if output_file is not None:
+            file_string += """
+      output_file: {output_dir}
+      """.format(output_dir=output_dir)
+        else:
+            file_string += """
+      output_dir: {output_dir}
+      output_pattern: '{output_pattern}'""".format(output_dir=output_dir, output_pattern=output_pattern)
+        if start_date is not None:
+            file_string += """
+      start_date: {start_date}""".format(start_date=start_date)
+        if end_date is not None:
+            file_string += """
+      end_date: {end_date}""".format(end_date=end_date)
+        return file_string
+
 
     def generate_impact(self, product, interval):
         _hazard_file = None
@@ -107,6 +175,8 @@ class ImpactConfigFactory(ConfigFactory.ConfigFactory):
         _output_dir = None
         _output_pattern = None
         filestring = None
+        _start_date = self.start_date
+        _end_date = self.end_date
         if product == 'vhi':
             _hazard_dir = self.vampire.get('MODIS_VHI', 'vhi_product_dir')
             _hazard_pattern = self.vampire.get('MODIS_VHI', 'vhi_pattern')
@@ -124,14 +194,34 @@ class ImpactConfigFactory(ConfigFactory.ConfigFactory):
                                                             hazard_pattern=_hazard_pattern, boundary_file=_boundary_file,
                                                             boundary_dir=_boundary_dir, boundary_pattern=_boundary_pattern,
                                                             boundary_field=_boundary_field, output_file=_output_file,
-                                                            output_dir=_output_dir, output_pattern=_output_pattern)
+                                                            output_dir=_output_dir, output_pattern=_output_pattern,
+                                                            start_date=_start_date, end_date=_end_date)
             _population_file = self.vampire.get_country(self.country)['population_layer']
             _output_pattern = self.vampire.get('hazard_impact', 'vhi_popn_output_pattern')
             filestring += self._generate_popn_impact_section(hazard_file=_hazard_file, hazard_dir=_hazard_dir,
-                                                            hazard_pattern=_hazard_pattern, boundary_file=_boundary_file,
-                                                            boundary_dir=_boundary_dir, boundary_pattern=_boundary_pattern,
-                                                            boundary_field=_boundary_field, population_file=_population_file,
-                                                            output_file=_output_file,
-                                                            output_dir=_output_dir, output_pattern=_output_pattern)
+                                                             hazard_pattern=_hazard_pattern, boundary_file=_boundary_file,
+                                                             boundary_dir=_boundary_dir, boundary_pattern=_boundary_pattern,
+                                                             boundary_field=_boundary_field, population_file=_population_file,
+                                                             output_file=_output_file,
+                                                             output_dir=_output_dir, output_pattern=_output_pattern,
+                                                             start_date=_start_date, end_date=_end_date)
+            _crop_boundary = self.vampire.get_country(self.country)['crop_boundary']
+            _crop_field = self.vampire.get_country(self.country)['crop_field']
+            _crop_dir = None
+            _crop_pattern = None
+            _admin_file = _boundary_file
+            _admin_dir = None
+            _admin_pattern = None
+            _admin_field = _boundary_field
+            _output_pattern = self.vampire.get('hazard_impact', 'vhi_crops_output_pattern')
+            filestring += self._generate_crop_impact_section(hazard_file=_hazard_file, hazard_dir=_hazard_dir,
+                                                             hazard_pattern=_hazard_pattern,
+                                                             crop_boundary=_crop_boundary, crop_dir=_crop_dir,
+                                                             crop_pattern=_crop_pattern, crop_field=_crop_field,
+                                                             admin_boundary=_admin_file, admin_boundary_dir=_admin_dir,
+                                                             admin_boundary_pattern=_admin_pattern,
+                                                             admin_field=_admin_field, output_file=_output_file,
+                                                             output_dir=_output_dir, output_pattern=_output_pattern,
+                                                             start_date=_start_date, end_date=_end_date)
 
         return filestring
