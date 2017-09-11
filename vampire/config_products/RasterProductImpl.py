454800af3d9e20d37d832eb7ca3caf4c7314e2fd
Rochelle O'Hagan <spatialexplore@gmail.com>
2017-09-11 20:29:22 +1000

    Renamed output_file, output_dir, output_pattern as product_file, product_dir, product_pattern.

--- vampire/config_products/RasterProductImpl.py
+++ vampire/config_products/RasterProductImpl.py
@@ -11,31 +11,31 @@ class RasterProductImpl(object):
         logger.debug('Initialising Raster Product')
         self.valid_from_date = None
         self.valid_to_date = None
-        self.output_file = None
-        self.output_dir = None
-        self.output_pattern = None
+        self.product_file = None
+        self.product_dir = None
+        self.product_pattern = None
         return
 
     @property
-    def output_file(self):
-        return self.__output_file
-    @output_file.setter
-    def output_file(self, of):
-        self.__output_file = of
+    def product_file(self):
+        return self.__product_file
+    @product_file.setter
+    def product_file(self, of):
+        self.__product_file = of
 
     @property
-    def output_dir(self):
-        return self.__output_dir
-    @output_dir.setter
-    def output_dir(self, od):
-        self.__output_dir = od
+    def product_dir(self):
+        return self.__product_dir
+    @product_dir.setter
+    def product_dir(self, od):
+        self.__product_dir = od
 
     @property
-    def output_pattern(self):
-        return self.__output_pattern
-    @output_pattern.setter
-    def output_pattern(self, op):
-        self.__output_pattern = op
+    def product_pattern(self):
+        return self.__product_pattern
+    @product_pattern.setter
+    def product_pattern(self, op):
+        self.__product_pattern = op
 
     @property
     def valid_from_date(self):
@@ -59,13 +59,13 @@ class RasterProductImpl(object):
       start_date: {start_date}
       end_date: {end_date}""".format(start_date=self.valid_from_date.strftime("%d/%m/%Y"),
                                      end_date=self.valid_to_date.strftime("%d/%m/%Y"))
-        if self.output_file is not None:
+        if self.product_file is not None:
             cfg_string += """
-      input_file: {input_file}""".format(input_file=self.output_file)
+      input_file: {input_file}""".format(input_file=self.product_file)
         else:
             cfg_string += """
       input_dir: {input_dir}
       input_pattern: '{input_pattern}'
-        """.format(input_dir=self.output_dir, input_pattern=self.output_pattern)
+        """.format(input_dir=self.product_dir, input_pattern=self.product_pattern)
 
         return cfg_string
