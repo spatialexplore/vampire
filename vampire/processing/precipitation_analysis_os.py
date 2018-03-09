Add debugging info. Bugfix - use masked array of zeros and after calculation fill with nodata value to eliminate default (1.0e+20) being used in flood_alert output.

vampire/processing/precipitation_analysis_os.py
@@ -303,18 +303,25 @@ def calc_flood_alert(forecast_filename, threshold_filename, dst_filename, value=
     _nodata = None
     with rasterio.open(forecast_filename) as forecast_r:
         _nodata = forecast_r.nodata
+        logger.debug('In calc_flood_alert: nodata value for forecast file is: {0}'.format(_nodata))
     tmp_output = os.path.join(os.path.dirname(forecast_filename), "tmpoutput.tif")
     raster_utils.reproject_image_to_master(threshold_filename, forecast_filename, tmp_output,
                                            nodata=_nodata)
+    if _nodata is None:
+        _nodata = -9999
 
     with rasterio.open(tmp_output) as forecast_r:
+        logger.debug('reprojected forecast file nodata={0}'.format(forecast_r.nodata))
         _forecast_band = forecast_r.read(1, masked=True)
         _profile = forecast_r.profile.copy()
         with rasterio.open(threshold_filename) as threshold_r:
+            logger.debug('threshold file nodata= {0}'.format(threshold_r.nodata))
             _threshold_band = threshold_r.read(1, masked=True)
-            _out_ras = np.zeros(shape=_threshold_band.shape)
+            _out_ras = np.ma.zeros(shape=_threshold_band.shape)
+            _out_ras = np.ma.masked_where(np.ma.getmask(_threshold_band), _out_ras)
             _out_ras[_forecast_band >= _threshold_band] += 1
-            _profile.update(dtype=rasterio.float64)
+            _out_ras = _out_ras.filled(_nodata)
+            _profile.update(dtype=rasterio.float64, nodata=_nodata)
             with rasterio.open(path=dst_filename, mode='w', **_profile) as dst:
                 dst.write(_out_ras.astype(rasterio.float64), 1)
 